ef929a534cd5e16f01510240795f85501d4e9ac1
Blupegasus0 <amireastmond@gmail.com>
2023-11-27 11:58:57 +0000

    added ghetto concurrency

--- file_train/src/main.rs
+++ file_train/src/main.rs
@@ -6,7 +6,8 @@ use chacha20poly1305::{
 use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::fs::File;
-// use std::path::Path;
+use std::sync::mpsc;
+use std::thread;
 
 pub mod server;
 pub mod client;
@@ -14,12 +15,23 @@ pub mod client;
 use crate::client::client::run_client;
 use crate::server::server::run_server;
 
+
+
 const BUFFER_SIZE: usize = 1024;
 const PORT: u16 = 3453;
 
 fn main() {
-    run_server();
-    run_client();
+    let server = thread::spawn(|| {
+        run_server();
+    });
+
+    let client = thread::spawn(|| {
+        run_client();
+    });
+
+
+    server.join().unwrap(); // HANDLE ERRORS
+    client.join().unwrap(); // HANDLE ERRORS
 }
 
 
