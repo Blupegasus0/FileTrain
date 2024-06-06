f4f8c4865e4f9fca13d892529b145f470ed275ca
Amir Eastmond <amireastmond@gmail.com>
2024-06-06 09:52:23 -0400

    more readable:w

--- FileTrainServer/src/server.rs
+++ FileTrainServer/src/server.rs
@@ -28,9 +28,13 @@ fn encrypt() {
     
     let key = secretbox::gen_key();
     let nonce = secretbox::gen_nonce();
+
     let plaintext = b"some data";
+
     let ciphertext = secretbox::seal(plaintext, &nonce, &key);
+
     let their_plaintext = secretbox::open(&ciphertext, &nonce, &key).unwrap();
+
     assert!(plaintext == &their_plaintext[..]);
 }
 
