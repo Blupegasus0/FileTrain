5e2bfd99c6d77faffc8f7b583e24b1ef86b0e2fc
Amir Eastmond <amireastmond@gmail.com>
2024-06-07 21:38:00 -0400

    debugging the mysterious 'hello?' message

--- FileTrainClient/src/client.rs
+++ FileTrainClient/src/client.rs
@@ -3,6 +3,10 @@ pub mod client {
     use std::io::prelude::*;
     use std::net::TcpListener;
 
+    const NONCE: usize = 12;
+    const KEY: usize = 32;
+    const BUFFER: usize = 1024;
+
 
     pub fn run_client () -> anyhow::Result<()> {
 
@@ -16,7 +20,7 @@ pub mod client {
         for stream in listener.incoming() {
             let mut stream = stream.unwrap();
 
-            let mut buffer = [0u8; 1024];
+            let mut buffer = [0u8; BUFFER];
             let read_count = stream.read(&mut buffer)?;
             
             let ciphertext_range = 0..read_count;
@@ -30,20 +34,22 @@ pub mod client {
     }
 
 
-    fn decrypt(ciphertext: &Vec<u8>) -> anyhow::Result<Vec<u8>> {
+    fn decrypt(payload: &Vec<u8>) -> anyhow::Result<Vec<u8>> {
         use chacha20poly1305::{
             aead::{Aead, AeadCore, KeyInit, OsRng},
             ChaCha20Poly1305, Nonce
         };
 
 
-        let key = [0u8; 32].as_ref().into();
-        let nonce = [0u8; 12].as_ref().into();
+        let key = [0u8; KEY].as_ref().into();
+        let nonce = &payload[..NONCE];
+        // let nonce = [0u8; NONCE].as_ref().into();
         let cipher = ChaCha20Poly1305::new(key);
 
+        let ciphertext = &payload[NONCE..];
         println!("{:?}",ciphertext);
 
-        let plaintext = cipher.decrypt(nonce, &ciphertext[..]).expect("decrypts ciphertext");
+        let plaintext = cipher.decrypt(nonce.into(), &ciphertext[..]).expect("decrypts ciphertext");
         Ok(plaintext)
     }
 } // mod client
--- FileTrainServer/src/server.rs
+++ FileTrainServer/src/server.rs
@@ -1,16 +1,18 @@
 pub mod server {
-
     use std::io::prelude::*;
     use std::net::TcpStream;
-
     use anyhow::Ok;
+
+    const NONCE: usize = 12;
+    const KEY: usize = 32;
 
 
     pub fn run_server() -> anyhow::Result<()> {
         let ip_addr = String::from("localhost:3453");
         let mut stream = TcpStream::connect(ip_addr)?;
 
-        let message = String::from("Hello World @ UWI");
+        // let message = String::from("Hello World @ UWI");
+        let message = String::from("Different test");
         let ciphertext = encrypt(&message)?;
 
         stream.write(&ciphertext)?;
@@ -19,23 +21,25 @@ pub mod server {
     }
 
     fn encrypt(plaintext: &String) -> anyhow::Result<Vec<u8>> {
-
         use chacha20poly1305::{
             aead::{Aead, KeyInit, OsRng},
             ChaCha20Poly1305,
         };
 
         // let key = ChaCha20Poly1305::generate_key(&mut OsRng);
-        let key = [0u8; 32].as_ref().into();
+        let key = [0u8; KEY].as_ref().into();
 
         let cipher = ChaCha20Poly1305::new(key);
+
         // let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
-        let nonce = [0u8; 12].as_ref().into();
+        let nonce = [0u8; NONCE].as_ref().into();
 
         let ciphertext = cipher.encrypt(nonce, plaintext.as_bytes()).expect("encrypts plaintext");
-        println!("{:?}", ciphertext);
 
-        Ok(ciphertext)
+        // add nonce as header
+        let mut payload = Vec::new(); payload.append(&mut nonce.to_vec()); payload.append(&mut ciphertext.to_vec());
+
+        Ok(payload)
     }
 
 } // mod server
